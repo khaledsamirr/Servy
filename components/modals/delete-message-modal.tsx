ef578614f324a9c6dfecce78810d0925e15cda98
Khaled Samir <66645843+khaledsamirr@users.noreply.github.com>
2023-10-03 06:33:25 +0300

    Final steps

---
+++ components/modals/delete-message-modal.tsx
@@ -0,0 +1,76 @@
+"use client";
+import{
+    Dialog,
+   DialogContent,
+   DialogDescription,
+   DialogFooter,
+   DialogHeader,
+   DialogTitle,
+   DialogTrigger 
+} from "@/components/ui/dialog";
+
+import qs from"query-string";
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+
+
+
+export const DeleteMessageModal=()=>{
+    
+   
+
+    const {isOpen,onClose,type,data}= useModal();
+    const {apiUrl,query}=data;
+    const isModalOpen= isOpen&&type==="deleteMessage";
+    const [isLoading,setIsLoading]=useState(false);
+    const onClick =async()=>{
+        try{
+            setIsLoading(true)
+            const url=qs.stringifyUrl({
+                url:apiUrl||"",
+                query
+            }
+
+            )
+
+            await axios.delete(url);
+            onClose();
+        }catch(err){
+            console.log(err)
+        }finally{
+            setIsLoading(false)
+        }
+    }
+    
+    return(
+        <Dialog open={isModalOpen} onOpenChange={onClose}>
+            <DialogContent className="bg-white text-black p-0 overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="text-2xl text-center font-bold">
+                    Delete Message </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        Are you sure you want to do this?<br/>the message will be permanently deleted.
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="b-gray-100 px-6 py-4 ">
+                    <div className="flex items-cente justify-between w-full">
+                        <Button disabled={isLoading}
+                        onClick={onClose} variant="ghost">
+                            Cancel
+                        </Button>
+                        <Button disabled={isLoading}
+                            variant="warning"
+                            onClick={onClick}
+                        >
+                            Confirm
+                        </Button>
+                    </div>
+                </DialogFooter>
+    
+            </DialogContent>
+        </Dialog>
+
+    )
+}

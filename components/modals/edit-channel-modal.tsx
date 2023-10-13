36869473ee8d7b695e76079925535288aeb3a8d8
Khaled Samir <66645843+khaledsamirr@users.noreply.github.com>
2023-10-13 06:31:26 +0300

    Update edit-channel-modal.tsx

--- components/modals/edit-channel-modal.tsx
+++ components/modals/edit-channel-modal.tsx
@@ -166,7 +166,7 @@ export const EditChannelModal = () => {
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
               <Button variant="primary" disabled={isLoading}>
-                Create
+                Save
               </Button>
             </DialogFooter>
           </form>
